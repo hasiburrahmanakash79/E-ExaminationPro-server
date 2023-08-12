104bb72ee22c97b664bc6a3deaf8ae28df175149
habib-N19 <habiburnabiarafat@gmail.com>
2023-08-13 00:09:18 +0600

    EEPS-1 added .env , added vercel config, added other cors config for smooth deploying to vercel>

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+import express, { json } from "express";
+import cors from "cors";
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// Middle ware
-app.use(cors());
-app.use(express.json());
+// Middleware
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+}
+app.use(cors(corsConfig))
+app.use(express.json())
+
+
+// mongo setup
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.ufcjidc.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get('/', (req, res) => {
   res.send('E-examPro')
