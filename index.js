a556a628cc6d728ae20e831c4882d6a7f8032e53
unknown <md.tazwarul.islam.07@gmail.com>
2023-08-28 22:55:19 +0600

    EEPS-14 <JWT Iplimentation>

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const cors = require('cors')
-
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
 
 const port = process.env.PORT || 5000
 
@@ -22,8 +23,6 @@ app.use(express.json())
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.ufcjidc.mongodb.net/?retryWrites=true&w=majority`
 
-
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -33,12 +32,37 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+/////////////JWT verify///////////
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    console.log('401-1')
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.SECRETE_TOKEN, (err, decoded) => {
+    if (err) {
+      console.log('401-2')
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     // Database collection 
+    const userCollection = client.db("E-ExaminationPro").collection("users");
     const shortQuestionCollection = client.db("E-ExaminationPro").collection("shortQuestions");
     const quizQuestionCollection = client.db("E-ExaminationPro").collection("quizQuestions");
     const fillInTheBlankCollection = client.db("E-ExaminationPro").collection("fillInTheBlanks")
@@ -52,10 +76,20 @@ async function run() {
 
     //////////////////////////////////////////////////////// Experiemnt (Abir)
 
+    app.post('/jwt', (req, res) => {
+      const userEmail=req.body;
+      console.log(userEmail)
+      const token= jwt.sign(userEmail,`${process.env.SECRETE_TOKEN}`, { expiresIn: '1h' })
+      res.send({token})
+  })
+
+
     const questionCollection= client.db("E-ExaminationPro").collection("Question_Collection")
     const subjectCollection= client.db("E-ExaminationPro").collection("allSubjects")
 
-    app.get('/allSubjects', async (req, res) => {
+    app.get('/allSubjects',verifyJWT ,async (req, res) => {
+      //const decoded=req.decoded
+     // console.log(decoded,'hit')
       const result = await subjectCollection.find().toArray();
       res.send(result);
     })
@@ -80,6 +114,8 @@ async function run() {
       res.send(result)
   })
     ///////////////////////////////////
+
+    
     app.get('/testimonials', async (req, res) => {
       const result = await testimonialCollection.find().toArray();
       res.send(result);
@@ -87,7 +123,7 @@ async function run() {
 
     app.get('/faqs', async (req, res) => {
       const result = await faqCollection.find().toArray();
-      res.send(result);
+      res.send(result)
     })
 
     app.get('/statistics', async (req, res) => {
