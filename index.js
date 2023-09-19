3e8c9a818407a67d81cf9a756205b518ee2bd3ba
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-09-19 10:55:48 +0600

    make a delete api for blog

--- index.js
+++ index.js
@@ -162,6 +162,13 @@ async function run() {
       const result = await blogsCollection.findOne(query);
       res.send(result);
     });
+    
+    app.delete("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     ///// JWT /////
     app.post("/jwt", (req, res) => {
